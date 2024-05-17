64a95a9be2968509e58ee1b4cc2b79d2f8e50200
Omar Jesús Maniás <ojmanias1985@gmail.com>
2024-05-16 22:47:01 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cartsRouter from './routes/carts.router.js'
+import productsRouter from './routes/products.router.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log(err)
+    }
+    )
+
+app.use('/api', cartsRouter)
+app.use('/api', productsRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
